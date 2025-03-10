aa5af314f58e4a030a46b4b649fbb9aba8d15158
Sepsepi <sepehr.moghaddamm@gmail.com>
2025-03-09 19:44:37 -0500

    added shit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.nickwalter.info','sepsepani.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.nickwalter.info','sepsepani.pythonanywhere.com', 'localhost', '127.0.0.1', '127.0.0.1:8000']
 
 
 # Application definition
@@ -123,10 +123,8 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
